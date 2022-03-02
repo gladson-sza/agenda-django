91e3ac932fe14d8b79b02fa0fb8016af93c0491f
gladson-sza <gladsonsouza@live.com>
2022-03-02 13:42:39 -0400

    Authentication included

--- core/views.py
+++ core/views.py
@@ -1,12 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from core.models import Event
 
 
 # Create your views here.
 
+def login_user(request):
+    return render(request, 'login.html')
 
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+        else:
+            messages.error(request, "User or password invalid")
+
+    return redirect('/')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+
+@login_required(login_url='/login')
 def list_events(request):
     events = Event.objects.all()
     response = {'events': events}
     return render(request, 'agenda.html', response)
-
